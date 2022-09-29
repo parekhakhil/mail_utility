de149f28ed8ca7a4255791a4952be0d5feb9b339
Akhil Parekh <parekhakhil11@gmail.com>
2022-09-29 13:35:37 +0530

    included user app and token authentication

--- mail_utility/settings.py
+++ mail_utility/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    
+    "users",
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
